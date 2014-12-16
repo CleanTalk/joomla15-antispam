d216cc131aff00645fbfe288bb78eefbff6a64c1
Denis Shagimuratov <poluxster@gmail.com>
2014-12-16 16:50:05 +0500

    Version updated.

--- antispambycleantalk.php
+++ antispambycleantalk.php
@@ -3,7 +3,7 @@
 /**
  * CleanTalk joomla plugin
  *
- * @version 2.5
+ * @version 2.6
  * @package Cleantalk
  * @subpackage Joomla
  * @author CleanTalk (welcome@cleantalk.ru) 
@@ -21,7 +21,7 @@ class plgSystemAntispambycleantalk extends JPlugin {
     /**
      * Plugin version string for server
      */
-    const ENGINE = 'joomla-25';
+    const ENGINE = 'joomla-26';
     
     /**
      * Default value for hidden field ct_checkjs 
